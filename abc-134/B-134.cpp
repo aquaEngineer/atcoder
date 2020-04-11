4e18a4429cddbce7647abf504d551529daeaa49c
aqua <aqua.server.tech@gmail.com>
2020-04-11 20:52:36 +0900

    abc-134

---
+++ abc-134/B-134.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <queue>
+#include <map>
+#include <utility>
+using namespace std;
+using ll = long long;
+ 
+int main() {
+	int a;
+	cin >> a;
+	cout << 3 * (a *a) << endl;
+	return 0;
+}
