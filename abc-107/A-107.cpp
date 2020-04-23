b5d715a0f9df9fc5dd2dc87289bf800b2657d000
aqua <aqua.server.tech@gmail.com>
2020-04-23 23:00:03 +0900

    abc-a

---
+++ abc-107/A-107.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <queue>
+#include <map>
+#include <utility>
+using namespace std;
+using ll = long long;
+ 
+int main() {
+	int n,i;
+	cin >> n >> i;
+	cout << n-i +1 << endl;
+	return 0;
+}
