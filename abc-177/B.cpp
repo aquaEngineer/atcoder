b7cc10596ce907943e319fbda40757313757a683
aqua <aqua.server.tech@gmail.com>
2020-09-25 09:54:46 +0900

    0925

---
+++ abc-177/B.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <queue>
+#include <map>
+#include <utility>
+#include <iomanip>  // std::setprecision()
+#include <set>
+
+using namespace std;
+using ll = long long;
+#define rep(i,a,b) for(ll i=(a); i<(b); i++)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define yes cout << "Yes" << endl;
+#define no cout << "No" << endl;
+#define pai 3.14159265358979323846
+ll gcd(ll a, ll b){if (a%b == 0)  return(b);else  return(gcd(b, a%b));}
+ll lcm(ll a, ll b){return a * b / gcd(a, b);}
+/* map<string,int> */
+/* 大文字を小文字に変換 tolower*/
+/* 小文字を大文字に変換 toupper*/
+/* 辞書順 next_permutation(a.begin(),a.end()) */
+void printVec(std::vector<char> &vec) {
+  std::cout << "";
+  for (auto it = vec.begin(); it != vec.end(); ++it) {
+    std::cout << *it << " ";
+  }
+  std::cout << std::endl;
+}
+ 
+int main() {
+    string s,t;
+    cin >> s >>t;
+    int count =0;
+    int mi = t.size();
+    for(int i =0;i < s.size()- t.size()+1;i++) {
+        count = 0;
+        for(int j =0; j < t.size(); j++) {
+            if (s[i+j] == t[j]) {
+                count++;
+            }
+        }
+        if (mi > (t.size() - count)){
+            mi = t.size() - count;
+        }
+    }
+    cout << mi << endl;
+}
---
+++ abc-178/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+        // if (ans > (ll)pow(10,9) + 7) {
+        //     cout << (ll) (n)  % ((ll) pow(10,9) + 7) << endl;
+        //     return 0;
+        // }
