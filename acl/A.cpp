7b1af13f56024777d12abc4852cfc48aa0517ec5
aqua <aqua.server.tech@gmail.com>
2020-09-27 23:22:51 +0900

    0927

---
+++ acl/A.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+using ll = long long;
+#define rep(i,a,b) for(ll i=(a); i<(b); i++)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define yes cout << "Yes" << endl;
+#define no cout << "No" << endl;
+#define pai 3.14159265358979323846
+ll gcd(ll a, ll b){if (a%b == 0)  return(b);else  return(gcd(b, a%b));}
+ll lcm(ll a, ll b){return a * b / gcd(a, b);}
+
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+/* map<string,int> */
+/* 大文字を小文字に変換 tolower*/
+/* 小文字を大文字に変換 toupper*/
+/* 辞書順 next_permutation(a.begin(),a.end()) */
+void printVec(std::vector<char> &vec) {
+  std::cout << "";
+  for (auto it = vec.begin(); it != vec.end(); ++it) {
+    std::cout << *it << " ";
+  }
+  std::cout << std::endl;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    rep(i,0,n) {
+        cout << "ACL";
+    }
+    cout << endl;
+}
