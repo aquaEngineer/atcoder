a22164fabd76f4aaadfa1b79f3f0e9288a125b5a
aqua <aqua.server.tech@gmail.com>
2020-10-06 23:54:36 +0900

    10/06

---
+++ abc-107/B.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h> 
+// #include <atcoder/all>
+
+using namespace std;
+// using namespace atcoder;
+using ll = long long;
+#define rep(i,a,b) for(ll i=(a); i<(b); i++)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define yes cout << "Yes" << endl;
+#define no cout << "No" << endl;
+#define pai 3.14159265358979323846
+ll gcd(ll a, ll b){if (a%b == 0)  return(b);else  return(gcd(b, a%b));}
+ll lcm(ll a, ll b){return a * b / gcd(a, b);}
+
+const int mod = 1000000007;
+/* map<string,int> */
+/* 大文字を小文字に変換 tolower*/
+/* 小文字を大文字に変換 toupper*/
+/* 辞書順 next_permutation(a.begin(),a.end()) */
+void printVec(std::vector<char> &vec) {
+  std::cout << "";
+  for (auto it = vec.begin(); it != vec.end(); ++it) {
+    std::cout << *it << " ";
+  }
+  std::cout << std::endl;
+}
+
+
+int main() {
+    int h,w;
+    cin >> h >> w;
+    vector <vector <int>> data(h, vector <int>(w));
+    char a;
+    rep(i,0,h) {
+        rep(j,0,w) {
+            cin >> a;
+            data[i][j] = (a == '#') ? true : false;
+        }
+    }
+    vector <bool> hs (h, true);
+    vector <bool> ws (w,true);
+    rep(i,0,w) {
+        rep (j,0,h) {
+            if (data[j][i] == true) {
+                ws[i] = false;
+                break;
+            }
+        }
+    }
+    rep(i,0,h) {
+        rep (j,0,w) {
+            if (data[i][j] == true) {
+                hs[i] = false;
+                break;
+            }
+        }
+    }
+
+    rep(i,0,h) {
+        if (hs[i]) continue;
+        rep(j,0,w) {
+            if (ws[j]) continue;
+            if (data[i][j]) {
+                cout << '#';
+            } else {
+                cout << '.';
+            }
+        }
+        cout << endl;
+    }
+}
---
+++ abc-129/C.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h> 
+// #include <atcoder/all>
+
+using namespace std;
+// using namespace atcoder;
+using ll = long long;
+#define rep(i,a,b) for(ll i=(a); i<(b); i++)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define yes cout << "Yes" << endl;
+#define no cout << "No" << endl;
+#define pai 3.14159265358979323846
+ll gcd(ll a, ll b){if (a%b == 0)  return(b);else  return(gcd(b, a%b));}
+ll lcm(ll a, ll b){return a * b / gcd(a, b);}
+
+const int mod = 1000000007;
+/* map<string,int> */
+/* 大文字を小文字に変換 tolower*/
+/* 小文字を大文字に変換 toupper*/
+/* 辞書順 next_permutation(a.begin(),a.end()) */
+void printVec(std::vector<char> &vec) {
+  std::cout << "";
+  for (auto it = vec.begin(); it != vec.end(); ++it) {
+    std::cout << *it << " ";
+  }
+  std::cout << std::endl;
+}
+
+
+int main() {
+    int n;
+    int m;
+    cin >> n >> m;
+    // vector <int> a(m);
+    map <int,int> ma;
+    int a;
+    rep(i,0,m) {
+        cin >> a;
+        ma[a] = a;
+    }
+    vector <int> b(n+1);
+    b[0] = 1;
+    if (ma.count(1) !=0) {
+        b[1] = 0;
+    } else {
+        b[1] = 1;
+    }
+    rep(i,2,n+1) {
+        b[i] = 0;
+        if (ma.count(i-1) == 0) {
+            b[i] += b[i-1];
+        } 
+        if (ma.count(i-2) == 0) {
+            b[i] += b[i -2];
+        }
+        b[i] %= mod;
+    }
+    cout << b[n]<< endl;
+
+}
